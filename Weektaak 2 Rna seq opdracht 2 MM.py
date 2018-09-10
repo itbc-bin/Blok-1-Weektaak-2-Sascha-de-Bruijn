60d5deb62ed58c0f9fca8aa3b9c9941f00b6e575
Saschadebruijn <43142830+Saschadebruijn@users.noreply.github.com>
2018-09-10 17:14:00 +0200

    Add files via upload

---
+++ Weektaak 2 Rna seq opdracht 2 MM.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+Seq = "" 
+Bestand = open("/home/sasnee/Documents/School Bio-informatica/Bio-informatica/Blok 1/Week 2/Opdracht 2/MMrna seq.txt", "r")
+for line in Bestand:
+    if not line.startswith(">"):
+        Seq = Seq + line
+print(Seq)
+
+print("Hieronder staat de volle sequentielengte")
+print(len(Seq))
+print("Hieronder staan alle basen in de volgorde ATGC")
+print(Seq.count("A"))
+print(Seq.count("T"))
+print(Seq.count("G"))
+print(Seq.count("C"))
+
+print("Hieronder staat het aantal G + C bij elkaar opgeteld")
+
+print(Seq.count("G") + (Seq.count("C")))
+print("Hieronder staat het GC percentage")
+GC_percentage = (((Seq.count("G") + (Seq.count("C"))) / len(Seq)) * 100)
+print(GC_percentage,"%")
+
+print("Mus musculus GC_percentage = 54.52%")
